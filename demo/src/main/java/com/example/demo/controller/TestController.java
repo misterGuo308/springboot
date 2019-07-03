aea9cc7b6c6084608aaf5b16f4e36129c019e973
guoyou <guoyou5378@139.com>
2019-07-03 23:53:06 +0800

    初始提交

---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.TestService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class TestController {
+    @Resource
+    private TestService testService;
+
+    @GetMapping(name = "/get")
+    public  String get(){
+
+        return testService.get();
+
+    }
+}
---
+++ demo/src/main/java/com/example/demo/service/impl/TestServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.mapper.TestMapper;
+import com.example.demo.service.TestService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class TestServiceImpl implements TestService {
+
+    @Resource
+    private TestMapper testMapper;
+    @Override
+    public String get() {
+        return testMapper.get();
+    }
+}
