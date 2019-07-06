3e5ce61b805fae521292b58b128580edd0727100
gxder <guoyou5378@126.com>
2019-07-06 15:55:48 +0800

    合并提交修改测试

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 
 @Controller
