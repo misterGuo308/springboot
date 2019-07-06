5fa9060ca97fcbe58eacd504e9951d3d41e3b822
gxder <guoyou5378@126.com>
2019-07-06 15:47:23 +0800

    主分支提交修改测试

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +10,13 @@ import javax.annotation.Resource;
 
 @RestController
 public class TestController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Resource
     private TestService testService;
     @GetMapping(name = "/get")
     public  String get(){
+      logger.info("测试日志");
         return testService.get();
 
     }
